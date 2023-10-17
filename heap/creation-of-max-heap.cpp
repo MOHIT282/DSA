f7e3300067e24034259025e2ac6b29bd0c06a37e
Mohit Kumar <kmohit0002@gmail.com>
2023-10-17 21:36:46 +0530

    added heapify algorithm

--- heap/creation-of-max-heap.cpp
+++ heap/creation-of-max-heap.cpp
@@ -63,24 +63,53 @@ class maxheap{
 };
 
 
+void heapify(int arr[], int n, int i){
+
+    int largest = i; 
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if(left < n && arr[largest] < arr[left]){
+        largest = left;
+    }
+    if(right < n && arr[largest] < arr[right]){
+        largest = right;
+    }
+
+    if(largest != i){
+        swap(arr[largest], arr[i]);
+        heapify(arr,n,largest);
+    }
+}
+
 int main(){
 
-    maxheap obj;
+    // maxheap obj;
 
-    obj.insert(54);
-    obj.insert(43);
-    obj.insert(50);
-    obj.insert(69);
-    obj.insert(20);
-    obj.insert(100);
-    obj.insert(78);
-    obj.insert(112);
+    // obj.insert(54);
+    // obj.insert(43);
+    // obj.insert(50);
+    // obj.insert(69);
+    // obj.insert(20);
+    // obj.insert(100);
+    // obj.insert(78);
+    // obj.insert(112);
 
-    obj.print();
+    // obj.print();
 
-    obj.deleteFromHeap();
-    obj.print();
-    obj.deleteFromHeap();
-    obj.print();
+    // obj.deleteFromHeap();
+    // obj.print();
+    // obj.deleteFromHeap();
+    // obj.print();
     
+    int arr[10] = {1, 23, 4,3,8, 10, 20,33, 78};
+    int n =  10;
+    for(int i = n/2; i>= 0; i--){
+        heapify(arr, n, i);
+    }
+
+    for(int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+    return 0;
 }
