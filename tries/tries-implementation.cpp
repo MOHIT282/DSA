4717ef527f630ea2770a81489f04dc2f7d64aa20
Mohit Kumar <kmohit0002@gmail.com>
2023-10-27 19:20:51 +0530

    implemented delete word function in tries

--- tries/tries-implementation.cpp
+++ tries/tries-implementation.cpp
@@ -23,23 +23,6 @@ class Trie{
 
     TrieNode *node;
 
-    bool searchUtil(TrieNode* root, string word){
-
-        //base case
-        if(word.length()== 0){
-            return root->isTerminal;
-        }
-            
-        int index = word[0] - 'A';
-
-        if(root->children[index]){
-            return searchUtil(root->children[index], word.substr(1));
-        }
-        else{
-            return false;
-        }
-    }
-
     void insertUtil(TrieNode *root, string word){
 
         if(word.length() == 0){
@@ -49,7 +32,7 @@ class Trie{
 
         // ek khud krlo baki recursion sambhal lega
 
-        int index = word[0] - 'A'; // it is assumed that all the letters would be capital
+        int index = word[0] - 'a'; // it is assumed that all the letters would be smaller
         TrieNode *child;
 
         if(root ->children[index]){ // children is already present 
@@ -63,16 +46,54 @@ class Trie{
 
         insertUtil(child, word.substr(1));
     }
+     
+    bool searchUtil(TrieNode* root, string word){
+
+        //base case
+        if(word.length()== 0){
+            return root->isTerminal;
+        }
+            
+        int index = word[0] - 'a';
+
+        if(root->children[index]){
+            return searchUtil(root->children[index], word.substr(1));
+        }
+        else{
+            return false;
+        }
+    }
 
     bool SearchPrefixUtil(TrieNode* root, string word){
             
         //base case
         if(word.length() == 0) return true;
 
-        int index = word[0] - 'A';
+        int index = word[0] - 'a';
 
         if(root->children[index]){
             return SearchPrefixUtil(root->children[index], word.substr(1));
+        }
+        else{
+            return false;
+        }
+    }
+
+    bool DeleteUtil(TrieNode* root, string word){
+
+        if(word.length() == 0){
+
+            if(root->isTerminal){
+                root->isTerminal = false;
+                return true;
+            }
+            return false;
+        }
+
+        int index = word[0] - 'a';
+
+        if(root->children[index]){
+            return DeleteUtil(root->children[index], word.substr(1));
         }
         else{
             return false;
@@ -85,24 +106,41 @@ class Trie{
             node = new TrieNode('\0'); // passed null character in the root
         }
 
-        void insertWord(string word) { insertUtil(node, word);}
+        void insertWord(string word) { insertUtil(node, word); }
 
-        bool searchWord(string word){return searchUtil(node,word);}
+        bool searchWord(string word){ return searchUtil(node,word); }
 
-        bool searchPrefix(string word){ return SearchPrefixUtil(node, word);}
+        bool searchPrefix(string word){ return SearchPrefixUtil(node, word); }
+
+        void deleteWord(string word) {
+
+            if(DeleteUtil(node, word)){
+                cout<<"Word "<<word<<" is deleted successfully"<<endl;
+            }
+            else{
+                cout<<"Word is not present or may not get deleted"<<endl;
+            }
+        }
 };
 
 int main(){
     Trie *t = new Trie();
 
-    t->insertWord("ABCD"); // passing lower case letters would give an error
+    vector<string> names = {"mohit","aayush","jatin","ritika","ruchika","anamika","rohan"};
 
-    cout<<"is ABCD present or not : "<<t->searchWord("ABCD")<<endl;
+    for(int i = 0; i < names.size(); i++){
+        t->insertWord(names[i]);
+    }
 
-    t->insertWord("MOHIT");
+    // t->insertWord("ABCD"); // passing lower case letters would give an error
+
+    cout<<"is abcd present or not : "<<t->searchWord("abcd")<<endl;
     
-    cout<<"is MOHIT present in the trie--> "<<t->searchWord("MOHIT")<<endl;
+    cout<<"is mohit present in the trie--> "<<t->searchWord("mohit")<<endl;
 
-    cout<<"is any word starting with MO present of not --> "<<t->searchPrefix("MO")<<endl;
+    cout<<"is any word starting with MO present of not --> "<<t->searchPrefix("mo")<<endl;
 
+    t->deleteWord("anamika");
+    t->deleteWord("anamika");
+    
 }
